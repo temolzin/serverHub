c654ddbd0c09bab0fcab6c7a1a435ee842452cff
ERICLOPEZRENDON <lopezrendoneric@gmail.com>
2026-01-21 22:20:46 -0600

    Add login, logout, and user registration

--- app/Http/Controllers/authentications/LoginBasic.php
+++ app/Http/Controllers/authentications/LoginBasic.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers\authentications;
 
+use Illuminate\Support\Facades\Auth;
+
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
@@ -10,5 +12,21 @@ class LoginBasic extends Controller
   public function index()
   {
     return view('content.authentications.auth-login-basic');
+  }
+  public function login(Request $request)
+  {
+    $credentials = $request->validate([
+      'email' => ['required', 'email'],
+      'password' => ['required'],
+    ]);
+
+    if (Auth::attempt($credentials)) {
+      $request->session()->regenerate();
+      return redirect()->intended('/');
+    }
+
+    return back()->withErrors([
+      'email' => 'Credenciales incorrectas',
+    ]);
   }
 }
