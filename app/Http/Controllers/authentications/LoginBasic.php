7877312febea58b20c421143ef5a546e05c6e6c6
ERICLOPEZRENDON <lopezrendoneric@gmail.com>
2026-01-26 20:20:50 -0600

    spaces removed

--- app/Http/Controllers/authentications/LoginBasic.php
+++ app/Http/Controllers/authentications/LoginBasic.php
@@ -4,11 +4,28 @@ namespace App\Http\Controllers\authentications;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class LoginBasic extends Controller
 {
   public function index()
   {
     return view('content.authentications.auth-login-basic');
+  }
+  public function login(Request $request)
+  {
+    $credentials = $request->validate([
+      'email' => ['required', 'email'],
+      'password' => ['required'],
+    ]);
+
+    if (Auth::attempt($credentials)) {
+      $request->session()->regenerate();
+      return redirect()->intended('/');
+    }
+
+    return back()->withErrors([
+      'email' => 'Credenciales incorrectas',
+    ]);
   }
 }
