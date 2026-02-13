cd0e5f655344dc73748f4ded8676a98052c8db67
ERICLOPEZRENDON <lopezrendoneric@gmail.com>
2026-02-12 23:13:41 -0600

    add changes to pagination

--- resources/views/gcp-machines/pagination.blade.php
+++ resources/views/gcp-machines/pagination.blade.php
@@ -1,13 +1,11 @@
 @if ($gcpMachines->hasPages())
   <nav aria-label="Page navigation">
     <ul class="pagination justify-content-end">
-
       <li class="page-item {{ $gcpMachines->onFirstPage() ? 'disabled' : '' }}">
         <a class="page-link" href="{{ $gcpMachines->previousPageUrl() }}">
           <i class="bx bx-chevron-left"></i>
         </a>
       </li>
-
       @for ($page = 1; $page <= $gcpMachines->lastPage(); $page++)
         <li class="page-item {{ $page == $gcpMachines->currentPage() ? 'active' : '' }}">
           <a class="page-link" href="{{ $gcpMachines->url($page) }}">
@@ -15,13 +13,11 @@
           </a>
         </li>
       @endfor
-
       <li class="page-item {{ $gcpMachines->hasMorePages() ? '' : 'disabled' }}">
         <a class="page-link" href="{{ $gcpMachines->nextPageUrl() }}">
           <i class="bx bx-chevron-right"></i>
         </a>
       </li>
-
     </ul>
   </nav>
 @endif
