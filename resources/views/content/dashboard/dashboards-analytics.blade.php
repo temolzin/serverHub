050aca064f4ff0eeccfdddab9058556455b4bf90
Edwin <alex1616mm@gmail.com>
2026-02-23 10:55:35 -0600

    corrections-of-dashbords

--- resources/views/content/dashboard/dashboards-analytics.blade.php
+++ resources/views/content/dashboard/dashboards-analytics.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts/contentNavbarLayout')
 
-@section('title', 'Panel de Control - Analytics')
+@section('title', 'Dashboard - Analytics')
 
 @section('vendor-style')
   @vite('resources/assets/vendor/libs/apex-charts/apex-charts.scss')
