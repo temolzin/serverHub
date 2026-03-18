e6801ecca55d48d43859fccf876bd8d157ac1989
ERICLOPEZRENDON <lopezrendoneric@gmail.com>
2026-03-18 15:52:51 -0600

    feat(menu): groups GCP and One-premises into Power On/Off submenus and fixes submenu rendering

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,7 +22,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        View::composer('*', function ($view) {
+        View::composer(['layouts.sections.menu.verticalMenu', 'layouts/sections/menu/verticalMenu'], function ($view) {
 
             if (!Auth::check()) {
                 return;
--- resources/views/layouts/sections/menu/submenu.blade.php
+++ resources/views/layouts/sections/menu/submenu.blade.php
@@ -1,52 +1,41 @@
 @php
-use Illuminate\Support\Facades\Route;
+    use Illuminate\Support\Facades\Route;
+    $depth = isset($depth) ? (int) $depth : 1;
 @endphp
 
 <ul class="menu-sub">
-  @if (isset($menu))
-    @foreach ($menu as $submenu)
+    @if (isset($menu))
+        @foreach ($menu as $submenu)
 
-    {{-- active menu method --}}
-    @php
-      $activeClass = null;
-      $active = 'active open';
-      $currentRouteName = Route::currentRouteName();
+            @php
+                $activeClass = '';
+                $active = 'active open';
+                $currentRouteName = Route::currentRouteName();
+                $hasSubmenu = isset($submenu['submenu']) && is_array($submenu['submenu']) && count($submenu['submenu']) > 0;
+                $submenuSlug = $submenu['slug'] ?? null;
+                $slugList = is_array($submenuSlug) ? $submenuSlug : (is_string($submenuSlug) ? [$submenuSlug] : []);
+                $isExactMatch = is_string($submenuSlug) && $currentRouteName === $submenuSlug;
+                $hasPrefixMatch = $hasSubmenu && collect($slugList)->contains(
+                    fn ($slug) => is_string($slug) && str_starts_with($currentRouteName, $slug)
+                );
+                $activeClass = $isExactMatch ? 'active' : ($hasPrefixMatch ? $active : '');
+            @endphp
 
-      if ($currentRouteName === $submenu->slug) {
-          $activeClass = 'active';
-      }
-      elseif (isset($submenu->submenu)) {
-        if (gettype($submenu->slug) === 'array') {
-          foreach($submenu->slug as $slug){
-            if (str_contains($currentRouteName,$slug) and strpos($currentRouteName,$slug) === 0) {
-                $activeClass = $active;
-            }
-          }
-        }
-        else{
-          if (str_contains($currentRouteName,$submenu->slug) and strpos($currentRouteName,$submenu->slug) === 0) {
-            $activeClass = $active;
-          }
-        }
-      }
-    @endphp
+            <li class="menu-item {{$activeClass}}">
+                <a href="{{ isset($submenu['url']) ? url($submenu['url']) : 'javascript:void(0)' }}" class="{{ $hasSubmenu ? 'menu-link menu-toggle' : 'menu-link' }}" @if (isset($submenu['target']) and !empty($submenu['target'])) target="_blank" @endif>
+                @if (isset($submenu['icon']))
+                    <i class="{{ $submenu['icon'] }}"></i>
+                @endif
+                <div>{{ isset($submenu['name']) ? __($submenu['name']) : '' }}</div>
+                @if (isset($submenu['badge']))
+                    <div class="badge rounded-pill bg-{{ $submenu['badge'][0] }} text-uppercase ms-auto">{{ $submenu['badge'][1] }}</div>
+                @endif
+                </a>
 
-      <li class="menu-item {{$activeClass}}">
-        <a href="{{ isset($submenu->url) ? url($submenu->url) : 'javascript:void(0)' }}" class="{{ isset($submenu->submenu) ? 'menu-link menu-toggle' : 'menu-link' }}" @if (isset($submenu->target) and !empty($submenu->target)) target="_blank" @endif>
-          @if (isset($submenu->icon))
-          <i class="{{ $submenu->icon }}"></i>
-          @endif
-          <div>{{ isset($submenu->name) ? __($submenu->name) : '' }}</div>
-          @isset($submenu->badge)
-            <div class="badge rounded-pill bg-{{ $submenu->badge[0] }} text-uppercase ms-auto">{{ $submenu->badge[1] }}</div>
-          @endisset
-        </a>
-
-        {{-- submenu --}}
-        @if (isset($submenu->submenu))
-          @include('layouts.sections.menu.submenu',['menu' => $submenu->submenu])
-        @endif
-      </li>
-    @endforeach
-  @endif
+                @if ($hasSubmenu && $depth < 2)
+                @include('layouts.sections.menu.submenu', ['menu' => $submenu['submenu'], 'depth' => $depth + 1])
+                @endif
+            </li>
+        @endforeach
+    @endif
 </ul>
