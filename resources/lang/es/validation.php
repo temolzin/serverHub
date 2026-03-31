8d90bbf0d5a43804d52dcc400c79a3b7c1045433
Edwin <alex1616mm@gmail.com>
2026-03-31 16:08:03 -0600

    corrections-new

---
+++ resources/lang/es/validation.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    'required' => 'El campo :attribute es obligatorio.',
+    'unique' => 'El :attribute ya ha sido registrado.',
+    'string' => 'El campo :attribute debe ser texto.',
+    'max' => [
+        'string' => 'El campo :attribute no debe exceder :max caracteres.',
+    ],
+
+    'attributes' => [
+        'hostname' => 'nombre del servidor',
+    ],
+
+];
