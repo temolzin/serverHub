6383bf850d6397fe10b4d358445b3216cb9d62a6
ERICLOPEZRENDON <lopezrendoneric@gmail.com>
2026-04-01 10:00:42 -0600

    refactor(users): migrate to DataTables, remove server-side search and paginate

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,21 +10,12 @@ class UserController extends Controller
 {
     public function index(Request $request)
     {
-        $search = $request->search;
-        $users = User::with('roles')
-            ->when($search, function ($query, $search) {
-                $query->where(function ($q) use ($search) {
-                    $q->where('name', 'like', "%{$search}%")
-                        ->orWhere('email', 'like', "%{$search}%");
-            });
-        })
-        ->paginate(10)
-        ->withQueryString();
+        $users = User::with('roles')->orderBy('name')->get();
         $permissions = Permission::all()->groupBy(function ($permission) {
             return explode(' ', $permission->name)[1] ?? 'General';
         });
-    return view('users.index', compact('users', 'permissions'));
-}
+        return view('users.index', compact('users', 'permissions'));
+    }
 
 
     public function store(Request $request)
