d598aaa98768e20c20509584140eebad07d82727
Edwin <alex1616mm@gmail.com>
2026-04-07 14:50:01 -0600

    corrections-new

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,5 +1,5 @@
 <div class="modal fade" id="editUserModal{{ $user->id }}" tabindex="-1">
-    <div class="modal-dialog modal-lg modal-dialog-centered">
+    <div class="modal-dialog modal-md modal-dialog-centered">
         <div class="modal-content">
             <form action="{{ route('users.update', $user) }}" method="POST">
                 @csrf
@@ -11,38 +11,36 @@
                     <button type="button" class="btn-close" data-bs-dismiss="modal"></button>
                 </div>
                 <div class="modal-body">
-                    <div class="row g-3">
-                        <div class="col-md-6">
-                            <div class="mb-3">
+                    <div class="row g-2">
+                        <div class="col-12">
+                            <div class="mb-2">
                                 <label class="form-label">
                                     <i class="bx bx-user me-1 text-primary"></i> Nombre
                                 </label>
                                 <input type="text" name="name" class="form-control"
                                     value="{{ old('name', $user->name) }}" required>
                             </div>
-                            <div class="mb-3">
+                            <div class="mb-2">
                                 <label class="form-label">
                                     <i class="bx bx-id-card me-1 text-primary"></i> Apellido
                                 </label>
                                 <input type="text" name="last_name" class="form-control"
                                     value="{{ old('last_name', $user->last_name) }}" required>
                             </div>
-                            <div class="mb-3">
+                            <div class="mb-2">
                                 <label class="form-label">
                                     <i class="bx bx-envelope me-1 text-primary"></i> Correo
                                 </label>
                                 <input type="email" name="email" class="form-control"
                                     value="{{ old('email', $user->email) }}" required>
                             </div>
-                            <div class="mb-3">
+                            <div class="mb-2">
                                 <label class="form-label">
                                     <i class="bx bx-lock me-1 text-primary"></i> Nueva Contraseña
                                 </label>
-                                <input type="password" name="password" class="form-control">
+                                <input type="password" name="password" class="form-control" placeholder="********">
                             </div>
-                        </div>
-                        <div class="col-md-6">
-                            <div class="border rounded-3 p-3 shadow-sm h-100">
+                            <div class="mb-2">
                                 <label class="form-label fw-semibold">
                                     <i class="bx bx-user-check me-1 text-primary"></i> Rol
                                 </label>
@@ -59,12 +57,8 @@
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-label-secondary" data-bs-dismiss="modal">
-                        Cancelar
-                    </button>
-                    <button type="submit" class="btn btn-primary">
-                        <i class="bx bx-save me-1"></i> Actualizar Usuario
-                    </button>
+                    <button type="button" class="btn btn-label-secondary" data-bs-dismiss="modal">Cancelar</button>
+                    <button type="submit" class="btn btn-primary"><i class="bx bx-save me-1"></i> Actualizar Usuario</button>
                 </div>
             </form>
         </div>
